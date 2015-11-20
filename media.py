f0374bb1252a33ebad998442d095f6500e1c9e36
Zeeshan Ahmad <ahmad.zeeshaan@gmail.com>
2015-11-20 21:42:03 +0300

    removed init call to parent object

--- media.py
+++ media.py
@@ -2,8 +2,7 @@ class Movie():
     """constructor for Movie taking arguments for movie title, description,
         trailer, poster, csat, release date"""
     def __init__(self, movie_title, movie_description, movie_poster,
-                movie_trailer, movie_release_date, movie_cast):
-        super(Movie, self).__init__()
+                movie_trailer, movie_release_date, movie_cast):        
         self.title = movie_title
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
